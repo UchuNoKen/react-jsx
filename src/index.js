9e92683a9b9bed5e052e4210b62ab2ec9637cde3
UchuNoKen <xen.o.fite@gmail.com>
2019-01-17 18:19:12 -0500

    Made use of htmlFor instead of for attribute.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const App = () => {
 
     return (
       <div>
-        <label className="label" for="name">
+        <label className="label" htmlFor="name">
           {labelText}
         </label>
         <input id="name" type="text" />
