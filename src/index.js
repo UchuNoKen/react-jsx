002c5dede012e4298c505e29e0494ccdbc65da6f
UchuNoKen <xen.o.fite@gmail.com>
2019-01-17 18:16:27 -0500

    Added label variable.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,12 @@ import ReactDOM from 'react-dom';
 // Create a react component
 const App = () => {
     const buttonText = { text: 'Click Me!' };
+    const labelText = 'Enter name: ';
 
     return (
       <div>
         <label className="label" for="name">
-          Enter name:
+          {labelText}
         </label>
         <input id="name" type="text" />
         <button style={{ backgroundColor: 'blue', color: 'white'}}>
